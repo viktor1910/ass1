976c8efd6768f5bf3e70a14a762b3e2646e21881
viktor <viktor@1on1english.vn>
2020-03-09 13:11:47 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function openMenu() {
+  document.getElementById("top-menu").style.visibility = "visible";
+  document.getElementById("menu-icon").style.visibility = "hidden";
+  document.getElementById("exit-menu-icon").style.visibility = "visible";
+}
+function closeMenu() {
+  document.getElementById("top-menu").style.visibility = "hidden";
+  document.getElementById("menu-icon").style.visibility = "visible";
+  document.getElementById("exit-menu-icon").style.visibility = "hidden";
+}
+
+const getPost = async () => {
+  const reps = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await reps.json();
+  renderPost(data);
+};
+
+const renderPost = data => {
+  const renderData = data.map(post => {
+    return `
+    <div>
+    <div class="single-post">
+      <h3>${post.title}</h3>
+      <p>${post.body}</p>
+      <p>User id: ${post.userId}</p>
+    </div>
+    <div>
+      <button>Edit</button>
+      <button>Delete</button>
+    </div></div>`
+    ;
+  });
+  document.getElementById("content").innerHTML = renderData.join("");
+};
+
+getPost();
